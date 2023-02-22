3b754b337dfaba9b4459759bc5313597d9f5a88d
Ralf Gommers <ralf.gommers@gmail.com>
2023-02-22 18:32:47 +0000

    Delete everything except numpy/core and numpy/random

--- numpy/exceptions.pyi
+++
@@ -1,8 +0,0 @@
-from numpy.exceptions import (
-    ComplexWarning as ComplexWarning,
-    ModuleDeprecationWarning as ModuleDeprecationWarning,
-    VisibleDeprecationWarning as VisibleDeprecationWarning,
-    TooHardError as TooHardError,
-    AxisError as AxisError,
-)
-
